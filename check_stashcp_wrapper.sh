2114d5bcef2e99d13496d8a0a98de9eb4bc48c16
Tony Aburaad <tony.aburaad@gmail.com>
2016-05-26 09:57:46 -0500

    third commit

--- check_stashcp_wrapper.sh
+++ check_stashcp_wrapper.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
     source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash
-    module load stashcp
+    module load stashcp/3.0
 
-    echo "starting wget"
-    time wget  http://stash.osgconnect.net/~dbala/2gb_file.tar 
+    #echo "starting wget"
+    #time wget  http://stash.osgconnect.net/~dbala/2gb_file.tar 
 
-    ls -l 2gb_file.tar  
-    rm -f 2gb_file.tar
+    #ls -l 2gb_file.tar  
+    #rm -f 2gb_file.tar
  
-   
+    echo "sleep for" $(( RANDOM % (10 - 5 + 1 ) + 5 )) "seconds"
+    sleep $(( RANDOM % (1800 + 1 ) ))
+    
     xrdcp --version
+
+    echo "xrdcp time"
+    time xrdcp -d 2 -f user/taburaad/public/2gb_file.tar /dev/null
     echo "starting stashcp"
-    START=$(date +%s)
+    #START=$(date +%s)
+    echo "closest server:"
     stashcp --closest 
-    time stashcp -d -r -s user/taburaad/public/2gb_file.tar -l .
+    time stashcp -r  user/taburaad/public/2gb_file.tar .
 
     ls -l 2gb_file.tar  
     rm -f 2gb_file.tar 
-    END=$(date +%s)
-    DIFF=$(( $END - $START ))
-    echo "StashCache took $DIFF seconds"
+    #END=$(date +%s)
+    #DIFF=$(( $END - $START ))
+    #echo "StashCache took $DIFF seconds"
    
 
