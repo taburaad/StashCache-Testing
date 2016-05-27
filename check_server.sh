acb6ec770777cd957fdb6346e02c1df8cc713d37
Tony Aburaad <tony.aburaad@gmail.com>
2016-05-27 15:11:11 -0500

    lol

--- check_server.sh
+++ check_server.sh
@@ -1,4 +1,19 @@
-
+echo "UCSD"
 time xrdcp -d 1 -f root://xrd-cache-1.t2.ucsd.edu:1094//user/taburaad/public/2gb_file.tar .
 ls -l 2gb_file.tar  
 rm -f 2gb_file.tar 
+
+echo "Nebraska"
+time xrdcp -d 1 -f root://hcc-stash.unl.edu:1094//user/taburaad/public/2gb_file.tar .
+ls -l 2gb_file.tar
+rm -f 2gb_file.tar
+
+echo "BNL-ATLAS"
+time xrdcp -d 1 -f root://osgxroot.usatlas.bnl.gov.edu:1094//user/taburaad/public/2gb_file.tar .
+ls -l 2gb_file.tar
+rm -f 2gb_file.tar
+
+echo "US MWT2"
+time xrdcp -d 1 -f root://mwt2-stashcache.campuscluster.illinois.edu:1094//user/taburaad/public/2gb_file.tar .
+ls -l 2gb_file.tar
+rm -f 2gb_file.tar
